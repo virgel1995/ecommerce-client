85fbc9ae1f7308d0be30cf24fa96ef8c4da08108
Sabry Dawood <72863390+virgel1995@users.noreply.github.com>
2023-03-03 03:39:40 +0000

    G

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,8 @@
 import api  from "./admin.js"
 import { apiUrl } from "./api.js"
 import {
-  HERO_URL,
-	ABOUT_IMG_URL,
+  HERO_Url,
+	ABOUT_IMG_Url,
 	BRANDS,
 	CATEGORIES,
 	API_URL
@@ -12,11 +12,11 @@ import {formatPrice} from "./helpers"
 
 
 
-export const HERO_URL =  HERO_URL;
-export const ABOUT_IMG_URL =  ABOUT_IMG_URL;
+export const HERO_URL =  HERO_Url;
+export const ABOUT_IMG_URL =  ABOUT_IMG_Url;
 export const BRANDS =  BRANDS;
 export const CATEGORIES =  CATEGORIES;
 export const API_URL =  API_URL;
 export const api = apiUrl
 export const adminApi = api;
-exports const formatPrice = formatPrice;
+export const formatPrice = formatPrice;
